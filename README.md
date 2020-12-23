d11ffb6dcf67f06704b271308c43b4d1300f5067
hajime OGUSHI <info@m-g-n.me>
2020-12-24 01:53:40 +0900

    1.0.1

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+アップデートされろー
+
 [![release](https://github.com/megane9988/megane-auto-update/workflows/Upload%20Release%20Asset/badge.svg)](https://github.com/megane9988/megane-auto-update/actions?query=workflow%3A%22Upload+Release+Asset%22)
 
 自動アップデートを目的としたプラグインです。
--- megane-update-ver2.php
+++ megane-update-ver2.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: 類人猿ブロックパターン（ベータ）test
  * Description: This plugin is a plugin with the sole purpose of being automatically updated.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires at least: 5.6
  * Requires PHP: 5.6
  * Requires Snow Monkey: 12.0.0
